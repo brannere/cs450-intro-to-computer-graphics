e1d9b27ff387a74bfdb4df4d252def9770da0223
Erick Branner <eobrann@gmail.com>
2020-10-18 17:59:15 -0700

    texture mapped; inside of sphere

--- project3-texture-mapping/sample.cpp
+++ project3-texture-mapping/sample.cpp
@@ -217,6 +217,166 @@ void	Visibility( int );
 
 void	Axes( float );
 void	HsvRgb( float[3], float [3] );
+bool	Distort;		// global -- true means to distort the texture
+
+struct point {
+	float x, y, z;		// coordinates
+	float nx, ny, nz;	// surface normal
+	float s, t;		// texture coords
+};
+
+int		NumLngs, NumLats;
+struct point *	Pts;
+
+struct point *
+PtsPointer( int lat, int lng )
+{
+	if( lat < 0 )	lat += (NumLats-1);
+	if( lng < 0 )	lng += (NumLngs-1);
+	if( lat > NumLats-1 )	lat -= (NumLats-1);
+	if( lng > NumLngs-1 )	lng -= (NumLngs-1);
+	return &Pts[ NumLngs*lat + lng ];
+}
+
+
+
+void
+DrawPoint( struct point *p )
+{
+	glNormal3f( p->nx, p->ny, p->nz );
+	glTexCoord2f( p->s, p->t );
+	glVertex3f( p->x, p->y, p->z );
+}
+
+void
+MjbSphere( float radius, int slices, int stacks )
+{
+	struct point top, bot;		// top, bottom points
+	struct point *p;
+
+	// set the globals:
+
+	NumLngs = slices;
+	NumLats = stacks;
+
+	if( NumLngs < 3 )
+		NumLngs = 3;
+
+	if( NumLats < 3 )
+		NumLats = 3;
+
+
+	// allocate the point data structure:
+
+	Pts = new struct point[ NumLngs * NumLats ];
+
+
+	// fill the Pts structure:
+
+	for( int ilat = 0; ilat < NumLats; ilat++ )
+	{
+		float lat = -M_PI/2.  +  M_PI * (float)ilat / (float)(NumLats-1);
+		float xz = cos( lat );
+		float y = sin( lat );
+		for( int ilng = 0; ilng < NumLngs; ilng++ )
+		{
+			float lng = -M_PI  +  2. * M_PI * (float)ilng / (float)(NumLngs-1);
+			float x =  xz * cos( lng );
+			float z = -xz * sin( lng );
+			p = PtsPointer( ilat, ilng );
+			p->x  = radius * x;
+			p->y  = radius * y;
+			p->z  = radius * z;
+			p->nx = x;
+			p->ny = y;
+			p->nz = z;
+			if( Distort )
+			{
+				p->s = 0;
+				p->t = 0;
+			}
+			else
+			{
+				p->s = ( lng + M_PI    ) / ( 2.*M_PI );
+				p->t = ( lat + M_PI/2. ) / M_PI;
+			}
+		}
+	}
+
+	top.x =  0.;		top.y  = radius;	top.z = 0.;
+	top.nx = 0.;		top.ny = 1.;		top.nz = 0.;
+	top.s  = 0.;		top.t  = 1.;
+
+	bot.x =  0.;		bot.y  = -radius;	bot.z = 0.;
+	bot.nx = 0.;		bot.ny = -1.;		bot.nz = 0.;
+	bot.s  = 0.;		bot.t  =  0.;
+
+
+	// connect the north pole to the latitude NumLats-2:
+
+	glBegin( GL_QUADS );
+	for( int ilng = 0; ilng < NumLngs-1; ilng++ )
+	{
+		p = PtsPointer( NumLats-1, ilng );
+		DrawPoint( p );
+
+		p = PtsPointer( NumLats-2, ilng );
+		DrawPoint( p );
+
+		p = PtsPointer( NumLats-2, ilng+1 );
+		DrawPoint( p );
+
+		p = PtsPointer( NumLats-1, ilng+1 );
+		DrawPoint( p );
+	}
+	glEnd( );
+
+	// connect the south pole to the latitude 1:
+
+	glBegin( GL_QUADS );
+	for( int ilng = 0; ilng < NumLngs-1; ilng++ )
+	{
+		p = PtsPointer( 0, ilng );
+		DrawPoint( p );
+
+		p = PtsPointer( 0, ilng+1 );
+		DrawPoint( p );
+
+		p = PtsPointer( 1, ilng+1 );
+		DrawPoint( p );
+
+		p = PtsPointer( 1, ilng );
+		DrawPoint( p );
+	}
+	glEnd( );
+
+
+	// connect the other 4-sided polygons:
+
+	glBegin( GL_QUADS );
+	for( int ilat = 2; ilat < NumLats-1; ilat++ )
+	{
+		for( int ilng = 0; ilng < NumLngs-1; ilng++ )
+		{
+			p = PtsPointer( ilat-1, ilng );
+			DrawPoint( p );
+
+			p = PtsPointer( ilat-1, ilng+1 );
+			DrawPoint( p );
+
+			p = PtsPointer( ilat, ilng+1 );
+			DrawPoint( p );
+
+			p = PtsPointer( ilat, ilng );
+			DrawPoint( p );
+		}
+	}
+	glEnd( );
+
+	delete [ ] Pts;
+	Pts = NULL;
+}
+
 
 // main program:
 
@@ -397,7 +557,11 @@ Display( )
 	// draw the current object:
 
 	glCallList( BoxList );
-
+		glEnable(GL_TEXTURE_2D);
+	glBindTexture( GL_TEXTURE_2D, Tex0 );
+	// glBegin( GL_QUADS );
+	MjbSphere(5,5,5);
+	glEnd( );
 	if( DepthFightingOn != 0 )
 	{
 		glPushMatrix( );
@@ -433,10 +597,11 @@ Display( )
 	glColor3f( 1., 1., 1. );
 	DoRasterString( 5., 5., 0., (char *)"Text That Doesn't" );
 
-	glEnable(GL_TEXTURE_2D);
-	glBindTexture( GL_TEXTURE_2D, Tex0 );
-	glBegin( GL_QUADS );
-	glEnd( );
+	// glEnable(GL_TEXTURE_2D);
+	// glBindTexture( GL_TEXTURE_2D, Tex0 );
+	// // glBegin( GL_QUADS );
+	// MjbSphere(10,10,10 );
+	// glEnd( );
 	// swap the double-buffered framebuffers:
 
 	glutSwapBuffers( );
@@ -764,56 +929,56 @@ InitLists( )
 	// create the object:
 
 	BoxList = glGenLists( 1 );
-	glNewList( BoxList, GL_COMPILE );
+	// glNewList( BoxList, GL_COMPILE );
 
-		glBegin( GL_QUADS );
+	// 	glBegin( GL_QUADS );
 
-			glColor3f( 0., 0., 1. );
-			glNormal3f( 0., 0.,  1. );
-				glVertex3f( -dx, -dy,  dz );
-				glVertex3f(  dx, -dy,  dz );
-				glVertex3f(  dx,  dy,  dz );
-				glVertex3f( -dx,  dy,  dz );
+	// 		glColor3f( 0., 0., 1. );
+	// 		glNormal3f( 0., 0.,  1. );
+	// 			glVertex3f( -dx, -dy,  dz );
+	// 			glVertex3f(  dx, -dy,  dz );
+	// 			glVertex3f(  dx,  dy,  dz );
+	// 			glVertex3f( -dx,  dy,  dz );
 
-			glNormal3f( 0., 0., -1. );
-				glTexCoord2f( 0., 0. );
-				glVertex3f( -dx, -dy, -dz );
-				glTexCoord2f( 0., 1. );
-				glVertex3f( -dx,  dy, -dz );
-				glTexCoord2f( 1., 1. );
-				glVertex3f(  dx,  dy, -dz );
-				glTexCoord2f( 1., 0. );
-				glVertex3f(  dx, -dy, -dz );
+	// 		glNormal3f( 0., 0., -1. );
+	// 			glTexCoord2f( 0., 0. );
+	// 			glVertex3f( -dx, -dy, -dz );
+	// 			glTexCoord2f( 0., 1. );
+	// 			glVertex3f( -dx,  dy, -dz );
+	// 			glTexCoord2f( 1., 1. );
+	// 			glVertex3f(  dx,  dy, -dz );
+	// 			glTexCoord2f( 1., 0. );
+	// 			glVertex3f(  dx, -dy, -dz );
 
-			glColor3f( 1., 0., 0. );
-			glNormal3f(  1., 0., 0. );
-				glVertex3f(  dx, -dy,  dz );
-				glVertex3f(  dx, -dy, -dz );
-				glVertex3f(  dx,  dy, -dz );
-				glVertex3f(  dx,  dy,  dz );
+	// 		glColor3f( 1., 0., 0. );
+	// 		glNormal3f(  1., 0., 0. );
+	// 			glVertex3f(  dx, -dy,  dz );
+	// 			glVertex3f(  dx, -dy, -dz );
+	// 			glVertex3f(  dx,  dy, -dz );
+	// 			glVertex3f(  dx,  dy,  dz );
 
-			glNormal3f( -1., 0., 0. );
-				glVertex3f( -dx, -dy,  dz );
-				glVertex3f( -dx,  dy,  dz );
-				glVertex3f( -dx,  dy, -dz );
-				glVertex3f( -dx, -dy, -dz );
+	// 		glNormal3f( -1., 0., 0. );
+	// 			glVertex3f( -dx, -dy,  dz );
+	// 			glVertex3f( -dx,  dy,  dz );
+	// 			glVertex3f( -dx,  dy, -dz );
+	// 			glVertex3f( -dx, -dy, -dz );
 
-			glColor3f( 0., 1., 0. );
-			glNormal3f( 0.,  1., 0. );
-				glVertex3f( -dx,  dy,  dz );
-				glVertex3f(  dx,  dy,  dz );
-				glVertex3f(  dx,  dy, -dz );
-				glVertex3f( -dx,  dy, -dz );
+	// 		glColor3f( 0., 1., 0. );
+	// 		glNormal3f( 0.,  1., 0. );
+	// 			glVertex3f( -dx,  dy,  dz );
+	// 			glVertex3f(  dx,  dy,  dz );
+	// 			glVertex3f(  dx,  dy, -dz );
+	// 			glVertex3f( -dx,  dy, -dz );
 
-			glNormal3f( 0., -1., 0. );
-				glVertex3f( -dx, -dy,  dz );
-				glVertex3f( -dx, -dy, -dz );
-				glVertex3f(  dx, -dy, -dz );
-				glVertex3f(  dx, -dy,  dz );
+	// 		glNormal3f( 0., -1., 0. );
+	// 			glVertex3f( -dx, -dy,  dz );
+	// 			glVertex3f( -dx, -dy, -dz );
+	// 			glVertex3f(  dx, -dy, -dz );
+	// 			glVertex3f(  dx, -dy,  dz );
 
-		glEnd( );
+	// 	glEnd( );
 
-	glEndList( );
+	// glEndList( );
 
 
 	// create the axes:
