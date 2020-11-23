5c6fa9da0dc00279ed40a4c2b00afe717541e1b7
Erick Branner <eobrann@gmail.com>
2020-11-22 23:19:31 -0800

    better looking curve

--- project6-geometric-modeling/linux/sample.cpp
+++ project6-geometric-modeling/linux/sample.cpp
@@ -576,24 +576,24 @@ Display()
 	// makePoints(&curves[3], 5);
 	// makePoints(&curves[4], 5);
 	// std::cout << &curves[0] << "\n";
-	curves[0].points[0].x0 = 1;
-	curves[0].points[0].y0 = 1;
+	curves[0].points[0].x0 = 0;
+	curves[0].points[0].y0 = 0;
 	curves[0].points[0].z0 = 0;
 	
 	curves[0].points[1].x0 = 2;
-	curves[0].points[1].y0 = 2;
+	curves[0].points[1].y0 = 1;
 	curves[0].points[1].z0 = 0;
 	
 	curves[0].points[2].x0 = 6;
-	curves[0].points[2].y0 = 6;
+	curves[0].points[2].y0 = 2;
 	curves[0].points[2].z0 = 0;
 	
-	curves[0].points[3].x0 = 9;
-	curves[0].points[3].y0 = 9;
+	curves[0].points[3].x0 = 7;
+	curves[0].points[3].y0 = 1;
 	curves[0].points[3].z0 = 0;
 
 	curves[0].points[4].x0 = 11;
-	curves[0].points[4].y0 = 11;
+	curves[0].points[4].y0 = 0;
 	curves[0].points[4].z0 = 0;
 
 
